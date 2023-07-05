482ce9096aa4ccd1759591d3859a29f7b3767c22
Arneo Marin-Vincetić <arneo.marinvincetic@gmail.com>
2023-07-05 07:21:28 +0200

    tabPage show data #1

--- src/components/pages/TabPage.jsx
+++ src/components/pages/TabPage.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import Tab from '../system/Tab';
 import Prism from "prismjs";
 import "../../styles/prism.css";
 import Table from '../system/Table';
 
 const TabPage = () => {
+    const [fileContent, setFileContent] = useState('');
+
+    useEffect(() => {
+        // Fetch the .txt file content when the component mounts
+        fetch('./components/system/tab/tab.txt')
+            .then(response => response.text())
+            .then(content => setFileContent(content))
+            .catch(error => console.log(error));
+    }, []);
+    console.log(fileContent)
     const data = [
         { cell1: 'type', cell2: 'Vrsta tipke', cell3: 'string', cell4: 'default' },
         { cell1: 'children', cell2: 'Sadržaj unutar tipke', cell3: 'node', cell4: '-' },
         { cell1: 'disabled', cell2: 'Zastavica za onemogućavanje tipke', cell3: 'bool', cell4: 'false' },
     ];
-    const options = ['JSX', 'SCSS', 'Copy'];
+    const options_1 = ['JSX', 'SCSS', 'Copy'];
+    const options_2 = ['JSX', 'SCSS', 'Copy'];
+    const tabContent = [
+        `${fileContent}`,
+        'Ovo je sadržaj Tab 2',
+        'Ovo je sadržaj Tab 3'
+    ];
+
     return (
         <div className="modular-page-structure">
             <h2>Tab</h2>
@@ -28,20 +45,38 @@ const TabPage = () => {
                 <div className="row">
                     <div className="padding col-5-lg">
                         <div className="column">
-                            <Tab options={options} />
+                            <Tab options={options_1} />
                         </div>
                     </div>
                     <div className="vertical-line"></div>
-                    <div className="col-5-lg"></div>
+                    <div className="col-5-lg">
+
+                        <div className="code-container">
+                            <Tab options={options_2} content={tabContent} />
+                        </div>
+
+                    </div>
                 </div>
                 
                 <div className="horizontal-line"></div>
 
                 <div className="padding">
                     <h3>Javascript</h3>
-                    <pre className="language-javascript"><code>{`
+                    {/* <pre className="language-javascript"><code>{``}</code></pre>  */}
+                    {`
+                        import React from 'react';
 
-`}</code></pre> 
+                        const MyComponent = () => {
+                            return (
+                            <div>
+                                <h1>Hello, World!</h1>
+                                <p>This is a JSX code example.</p>
+                            </div>
+                            );
+                        };
+
+                        export default MyComponent;
+                    `}
                 </div>   
             </div>
 
--- src/components/system/Tab.jsx
+++ src/components/system/Tab.jsx
@@ -1,39 +1,34 @@
 import React, { useState } from 'react';
 import '../../styles/scss/system/_tab.scss';
 
-// Definiranje komponente Tab koja prihvaća opcije za tabove
-const Tab = ({ options = [] }) => {
-  // Definiranje stanja za aktivni tab i funkcije za postavljanje stanja
+const Tab = ({ options = [], content = [] }) => {
   const [activeTab, setActiveTab] = useState(0);
+  const [activeContent, setActiveContent] = useState(content[0]);
 
-  // Funkcija koja se poziva prilikom klika na tab i postavlja aktivni tab na odabrani indeks
   const handleTabClick = (index) => {
     setActiveTab(index);
+    setActiveContent(content[index]);
   };
 
   return (
-    // Kontejner za tabove
     <div className="tab-container">
       <div className="tab-row">
-        {/* Iteracija kroz opcije i generiranje tabova */}
         {options &&
           options.map((option, index) => (
             <div
-              key={index} // Jedinstveni ključ za svaki tab
-              className={`tab-option ${activeTab === index ? 'active' : ''}`} // Postavljanje klase 'active' za trenutno odabrani tab
-              onClick={() => handleTabClick(index)} // Event listener za klik na tab koji poziva handleTabClick funkciju
+              key={index}
+              className={`tab-option ${activeTab === index ? 'active' : ''}`}
+              onClick={() => handleTabClick(index)}
             >
               {option}
             </div>
           ))}
+      </div>
+      <div className="tab-content">
+        <pre className="language-javascript"><code>{activeContent}</code></pre> 
       </div>
     </div>
   );
 };
 
 export default Tab;
-
-
-
-
-
