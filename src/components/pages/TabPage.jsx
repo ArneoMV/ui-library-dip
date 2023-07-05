d2fd8d7be5d7cffd33ae5ee532d4032ddbff6ded
Arneo Marin-Vincetić <arneo.marinvincetic@gmail.com>
2023-07-06 00:48:51 +0200

    tab component

--- src/components/pages/TabPage.jsx
+++ src/components/pages/TabPage.jsx
@@ -1,31 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import Tab from '../system/Tab';
-import Prism from "prismjs";
-import "../../styles/prism.css";
 import Table from '../system/Table';
-
+import tab_jsx from '../system/tab/tab_jsx.txt';
+import tab_scss from '../system/tab/tab_scss.txt';
+ 
 const TabPage = () => {
     const [fileContent, setFileContent] = useState('');
     const [secondTabContent, setSecondTabContent] = useState('');
-
+ 
     useEffect(() => {
-        fetchTextContent('./tab/tab_jsx', setFileContent);
+        fetchTextContent(tab_jsx, setFileContent);
+        fetchTextContent(tab_scss, setSecondTabContent);
     }, []);
-
+ 
     const fetchTextContent = (url, setContent) => {
         fetch(url)
-            .then(response => response.text())
-            .then(content => setContent(content))
-            .catch(error => console.log(error));
+          .then((response) => response.text())
+          .then((content) => setContent(content))
+          .catch((error) => console.log(error));
     };
-
-    const handleTabClick = (tabIndex) => {
-        if (tabIndex === 1 && !secondTabContent) {
-            fetchTextContent('./tab/tab_scss', setSecondTabContent);
-        }
-    };
-
-    const data = [
+ 
+    // const handleTabClick = (tabIndex) => {
+    //     if (tabIndex === 1 && !secondTabContent) {
+    //         fetchTextContent('./system/tab/tab_scss.txt', setSecondTabContent);
+    //     }
+    // };
+ 
+    const tableData = [
         { cell1: 'type', cell2: 'Vrsta tipke', cell3: 'string', cell4: 'default' },
         { cell1: 'children', cell2: 'Sadržaj unutar tipke', cell3: 'node', cell4: '-' },
         { cell1: 'disabled', cell2: 'Zastavica za onemogućavanje tipke', cell3: 'bool', cell4: 'false' },
@@ -37,7 +38,7 @@ const TabPage = () => {
         secondTabContent,
         'Ovo je sadržaj Tab 3'
     ];
-
+ 
     return (
         <div className="modular-page-structure">
             <h2>Tab</h2>
@@ -48,10 +49,10 @@ const TabPage = () => {
                 <li>Kada trebate potvrditi polja u određenim pravilima.</li>
             </ul>
             <h3>Kada korisiti formu</h3>
-
+ 
             {/* Primjeri */}
             <div className="example-section column">
-
+ 
                 <div className="column">
                     <div className="padding col-5-lg">
                         <div className="column">
@@ -60,21 +61,21 @@ const TabPage = () => {
                     </div>
                     <div className="horizontal-line"></div>
                     <div className="padding col-12-lg">
-
+ 
                         <div className="code-container">
                             <Tab options={options_2} content={tabContent} />
                         </div>
-
+ 
                     </div>
                 </div>
-                
-  
+ 
+ 
             </div>
-
+ 
             {/* Table */}
-            <Table data={data} />
+            <Table data={tableData} />
         </div>
     );
 };
-
+ 
 export default TabPage;
